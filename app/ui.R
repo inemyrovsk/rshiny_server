286c9e5a31ff18586713ba9919b4d700dd925510
Ivan Nemyrovskiy <i.nemyrovskiy@netframe.org>
2022-05-26 21:38:43 +0300

    app

--- app/ui.R
+++ app/ui.R
@@ -1,7 +1,27 @@
-basicPage(
-  plotOutput('plot', width = "300px", height = "300px"),
-  tableOutput('table'),
-  radioButtons('style', 'Progress bar style', c('notification', 'old')),
-  actionButton('goPlot', 'Go plot'),
-  actionButton('goTable', 'Go table')
+#' Shiny app server object
+#'
+#' @importFrom graphics hist
+#' @import shiny
+
+# create the shiny application user interface
+shinyAppUI <- fluidPage(
+
+  # Application title
+  titlePanel("Old Faithful Geyser Data"),
+
+  # Sidebar with a slider input for number of bins
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("bins",
+                  "Number of bins:",
+                  min = 1,
+                  max = 50,
+                  value = 30)
+    ),
+
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("distPlot")
+    )
+  )
 )
